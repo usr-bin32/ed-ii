6c397c987d4ff7caf064a4ffd06e5e991ca7ee04
Gabriel <71942269+usr-bin32@users.noreply.github.com>
2020-10-19 13:17:22 -0300

    refatorar código

--- src/algorithms/shell_sort.h
+++ src/algorithms/shell_sort.h
@@ -1,38 +1,42 @@
 #include <cstdlib>
+
 /**
  * Implementção do Algoritmo escolhido para fazer a ordenacao
 **/
 template <typename T>
-void shell_sort(T *arr,  size_t size, int &comparisonsCounter, int &changesCounter) 
-{ 
+void shell_sort(T *arr, size_t size, int &comparisons, int &swaps) {
+    comparisons = 0;
+    swaps = 0;
+
     size_t i, j, h = 1;
     T aux;
 
-    do { h = h * 3 + 1; } while (h < size); // calcula o tamanho do salto h inicial
-                                       
-    do
-    {
-        h /= 3;  // atualiza o valor de h.
-        for(i = h ; i < size ; i++) 
-        {
+    // calcula o tamanho do salto h inicial
+    do {
+        h = h * 3 + 1;
+    } while (h < size);
+
+    do {
+        // atualiza o valor de h.
+        h /= 3;
+        for (i = h; i < size; i++) {
             aux = arr[i];
             j = i;
 
-            //efetua comparações entre elementos com distância h:
-            while (arr[j - h] > aux) 
-            {
-                comparisonsCounter++;
+            // efetua comparações entre elementos com distância h:
+            while (arr[j - h] > aux) {
+                comparisons++;
                 arr[j] = arr[j - h];
-                changesCounter++;
+                swaps++;
                 j -= h;
 
-                if (j < h) break;
+                if (j < h)
+                    break;
             }
-            comparisonsCounter++;
+            comparisons++;
 
             arr[j] = aux;
-            changesCounter++;
+            swaps++;
         }
     } while (h != 1);
-
-};
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,49 @@
 #include <algorithm>
-#include <array>
-#include <cstring>
 #include <chrono>
 #include <fstream>
 #include <iostream>
-#include <numeric>
 #include <random>
-#include <sstream>
 #include <string>
 #include <vector>
 
-#include "structures/book.h"
-#include "parsing/csv_parser.h"
-#include "algorithms/merge_sort.h"
 #include "algorithms/quick_sort.h"
 #include "algorithms/shell_sort.h"
+#include "parsing/csv_parser.h"
+#include "structures/book.h"
+
+using sort_function = void (*)(book *, size_t, int &, int &);
+
+const int NUM_TESTS = 5;
 
 void read_input(std::vector<unsigned int> &sizes);
 void read_books(std::vector<book> &books);
 void randomize(std::vector<book> &books);
 
-int main()
-{
-    int changesCounter = 0;
-    int comparisonsCounter = 0;
+void test_sort(sort_function sort_function, std::vector<book> &vector, size_t size);
 
+int main() {
     std::vector<unsigned int> sizes;
     read_input(sizes);
 
-    if (sizes.empty())
-    {
+    if (sizes.empty()) {
         return 0;
     }
 
     std::vector<book> books;
     read_books(books);
 
-    for (auto size : sizes)
-    {
-        std::cout << std::endl;
-        std::cout << "Testando N: " << size << std::endl;
-        int totalComparisons = 0;
-        int totalChanges = 0;
-        double totalTimes = 0;
+    for (auto size : sizes) {
+        std::cout << "N = " << size << std::endl;
 
-        for (int i = 0; i < 5; i++)
-        {
+        std::cout << "Shell Sort:" << std::endl;
+        test_sort(static_cast<sort_function>(&shell_sort), books, size);
 
-            randomize(books);
-            double t0 = double(clock()) / CLOCKS_PER_SEC;
-            shell_sort(books.data(), size, comparisonsCounter, changesCounter);
-            double t1 = double(clock()) / CLOCKS_PER_SEC;
-
-            totalComparisons += comparisonsCounter;
-            totalChanges += changesCounter;
-            totalTimes += t1 - t0;
-
-            changesCounter = 0;
-            comparisonsCounter = 0;
-        }
-
-        std::cout << std::endl;
-        std::cout << "Shell comparacoes: " << totalComparisons / 5 << std::endl;
-        std::cout << "Shell trocas: " << totalChanges / 5 << std::endl;
-        std::cout << "Shell tempo: " << totalTimes / 5 << std::endl;
-
-        totalComparisons = 0;
-        totalChanges = 0;
-        totalTimes = 0;
-        
-        for (int i = 0; i < 5; i++)
-        {
-
-            randomize(books);
-
-            double t0 = double(clock()) / CLOCKS_PER_SEC;
-            quick_sort(books.data(), 0, size - 1, comparisonsCounter, changesCounter);
-            double t1 = double(clock()) / CLOCKS_PER_SEC;
-
-            totalComparisons += comparisonsCounter;
-            totalChanges += changesCounter;
-            totalTimes += t1 - t0;
-
-            changesCounter = 0;
-            comparisonsCounter = 0;
-        }
-
-        std::cout << std::endl;
-        std::cout << "Quick comparacoes: " << totalComparisons / 5 << std::endl;
-        std::cout << "Quick trocas: " << totalChanges / 5 << std::endl;
-        std::cout << "Quick tempo: " << totalTimes / 5 << std::endl;
+        std::cout << "Quick Sort" << std::endl;
+        test_sort(static_cast<sort_function>(&quick_sort), books, size);
     }
 }
 
-void read_input(std::vector<unsigned int> &sizes)
-{
+void read_input(std::vector<unsigned int> &sizes) {
     std::ifstream input_file("./entrada.txt");
 
     if (!input_file.is_open())
@@ -103,28 +52,20 @@ void read_input(std::vector<unsigned int> &sizes)
         return;
     }
 
-    std::string line;
-    std::getline(input_file, line);
-
     unsigned int len = 0;
-    std::istringstream iss(line);
-    iss >> len;
+    input_file >> len;
 
     sizes.reserve(len);
     for (int i = 0; i < len; i++)
     {
-        std::getline(input_file, line);
-        std::istringstream iss(line);
-
         unsigned int size = 0;
-        iss >> size;
+        input_file >> size;
 
         sizes.push_back(size);
     }
 }
 
-void read_books(std::vector<book> &books)
-{
+void read_books(std::vector<book> &books) {
     csv_parser parser("./res/data.csv");
 
     if (!parser.is_open())
@@ -147,4 +88,30 @@ void randomize(std::vector<book> &books)
 {
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::shuffle(books.begin(), books.end(), std::default_random_engine(seed));
+}
+
+void test_sort(sort_function sort, std::vector<book> &vector, size_t size) {
+    int total_comparisons = 0;
+    int total_swaps = 0;
+    double total_time = 0;
+
+    for (int i = 0; i < NUM_TESTS; i++)
+    {
+        randomize(vector);
+
+        int comparisons;
+        int swaps;
+
+        double t0 = double(clock()) / CLOCKS_PER_SEC;
+        sort(vector.data(), size, comparisons, swaps);
+        double t1 = double(clock()) / CLOCKS_PER_SEC;
+
+        total_comparisons += comparisons;
+        total_swaps += swaps;
+        total_time += t1 - t0;
+    }
+
+    std::cout << "    Comparações: " << total_comparisons / NUM_TESTS << std::endl;
+    std::cout << "    Trocas: " << total_swaps / NUM_TESTS << std::endl;
+    std::cout << "    Tempo: " << total_time / NUM_TESTS << " s" << std::endl;
 }
