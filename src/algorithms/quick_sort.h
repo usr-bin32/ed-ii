0b0131a41acdd4f2a76e818f1a89bee34ebc762f
Gabriel <71942269+usr-bin32@users.noreply.github.com>
2020-10-22 19:44:09 -0300

    traduzir comentários

--- src/algorithms/quick_sort.h
+++ src/algorithms/quick_sort.h
@@ -1,7 +1,8 @@
 #include <cstdlib>
 
+// particiona o vetor e o rearranja de forma que todos os elementos anteriores ao pivô sejam menores que ele e todos os posteriores sejam maiores.
 template <typename T>
-int slice(T *arr, int first, int end, int &comparisons, int &swaps) { #particiona o vetor e o rearranja de forma que todos os elementos anteriores ao pivô sejam menores que ele e todos os posteriores sejam maiores.
+int slice(T *arr, int first, int end, int &comparisons, int &swaps) {
     int n = (end + first);
     T pivot = arr[n / 2];
 
@@ -32,8 +33,10 @@ template <typename T>
 void quick_sort(T *arr, int first, int end, int &comparisons, int &swaps) {
     if (first < end) {
         int s = slice(arr, first, end, comparisons, swaps);
-        quick_sort(arr, first, s, comparisons, swaps); #chamada recursiva para ordenar o subvetor de elementos menores
-        quick_sort(arr, s + 1, end, comparisons, swaps); #chamada recursiva para ordenar o subvetor de elementos maiores
+        // chamada recursiva para ordenar o subvetor de elementos menores
+        quick_sort(arr, first, s, comparisons, swaps);
+        // chamada recursiva para ordenar o subvetor de elementos maiores
+        quick_sort(arr, s + 1, end, comparisons, swaps);
     }
 }
 
